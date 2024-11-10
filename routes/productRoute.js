0ffa6da1ab48f2a544befb3cf28344bdf2becd23
healthcareoptical <healthcareopticalhk@gmail.com>
2024-11-10 11:43:39 -0500

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as productConrtoller from '../controllers/productController.js';
+import { upload } from "../middlewares/uploadFile";
 
 const productRoute = Router();
 
 //Handle user by productConrtoller.login
 productRoute.route('/')
-    .post(productConrtoller.createProduct)
+    .post(upload.single(file), productConrtoller.createProduct)
     .patch(productConrtoller.updateProduct)
     .get(productConrtoller.getProducts)
     .delete(productConrtoller.deleteProduct);
