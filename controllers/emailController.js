5ed78135ca8c43afe1ab4cba53efd098100ecd70
chanw <wchan194@my.centennialcollege.ca>
2024-12-06 17:08:29 -0500

    Update emailController.js

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -8,15 +8,22 @@ export async function sendEmail(req, res, next) {
     const message = req.body.message;
 
     try {
-        resend.emails.send({
+        const { data, error } = await resend.emails.send({
             from: 'onboarding@resend.dev',
             to: 'healthcareopticalhk@gmail.com',
             subject: subject,
             html: message
         });
+
+        if (error){
+            res.errorMessage = error.message;
+            res.errorCode = error.statusCode;
+            throw new Error('');
+        }
     } catch (error) {
-        response.errorMessage = response.errorMessage || "Error Occurs";
-        response.errorCode = response.errorCode || 500;
+        const response = {};
+        response.errorMessage = res.errorMessage || "Error Occurs";
+        response.errorCode = res.errorCode || 500;
         return next(response);
     }
     
